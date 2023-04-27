dcc355cefd40006a9b2ce4b133232149468b5d18
Jonggwan Park <30569896+little7279@users.noreply.github.com>
2023-04-28 00:26:57 +0900

    preparing Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.10.9-slim-bullseye
+LABEL authors="Jonggwan Park <craftguy55@gmail.com>"
+LABEL description="Dockerfile for building a Python 3.10.9 image with pipenv"
+LABEL version="1.0.0"
+
+WORKDIR /back
+#ENV PYTHONUNBUFFERED=1
+#ENV PYTHONPATH=/back
+
+COPY requirements.txt .
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
+
+EXPOSE 8000
+
+ENTRYPOINT ["python", "/back/main.py"]
+
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-from sql_app import database
+# from sql_app import database
 
 app = FastAPI()
 
+origins = [
+    "https://jonngwanma.de"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -13,3 +25,17 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # engine = create_engine(
+    #     get_settings().db_url,
+    # )
+    # Base.metadata.create_all(engine)
+
+    # uvicorn run for production
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+                # ssl_keyfile="/etc/ssl/private/privkey.pem",
+                # ssl_certfile="/etc/ssl/certs/fullchain.pem")
